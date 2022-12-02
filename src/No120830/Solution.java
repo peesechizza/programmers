1ff0febc2af39e3066d8e5f58fc62b40ea455647
a1dotls <meeae9408@gmail.com>
2022-12-02 18:30:28 +0900

    양꼬치 음료수 가격 구하기 enum 사용

--- src/No120830/Solution.java
+++ src/No120830/Solution.java
@@ -2,17 +2,43 @@ package No120830;
 
 public class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        int beveragePrice = 0;
-        int lambPrice = 0;
+       int lambTotalPrice = totalPrice(Menu.LAMB, n);
+       int beverageTotalPrice = totalPrice(Menu.BEVERAGE, k);
+       int discountPrice = discount(Menu.BEVERAGE, n);
 
-        lambPrice = n * 12000;
-        beveragePrice = k * 2000;
+       int totalPay = lambTotalPrice + beverageTotalPrice - discountPrice;
 
-        if (n >= 10) {
-            beveragePrice = (k * 2000) - ((n/10) * 2000);
-        }
+       return totalPay;
+    }
 
-        return lambPrice + beveragePrice;
+    private int totalPrice(Menu menu, int quantity) {
+        return menu.getPrice() * quantity;
+    }
+
+    private int discount(Menu menu, int lambQuantity) {
+        int point = lambQuantity / 10;
+        return menu.getPrice() * point;
+    }
+}
+
+enum Menu {
+
+    LAMB("양꼬치", 12000),
+    BEVERAGE("음료수", 2000);
+
+    private final String name;
+    private final int price;
+
+    Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
