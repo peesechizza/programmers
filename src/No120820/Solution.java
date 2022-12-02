d77526226750dc0eb6fd95938f97653e5b326160
a1dotls <meeae9408@gmail.com>
2022-12-02 17:19:06 +0900

    올해 기준 출생연도 구하기 기준 연도 LocalDate 사용

--- src/No120820/Solution.java
+++ src/No120820/Solution.java
@@ -1,15 +1,18 @@
 package No120820;
 
+import java.time.LocalDate;
+
 public class Solution {
     public int solution(int age) {
         int answer = 0;
         int maxRange = 120;
         int minRange = 0;
         boolean inputValidate = age > 0 && age <= 120;
-        int baseYear = 2022;
+
+        LocalDate today = LocalDate.now();
 
         if (inputValidate) {
-            answer = baseYear - age + 1;
+            answer = today.getYear() - age + 1;
         }
 
         return answer;
