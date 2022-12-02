ad90fb3f6671a5c72d3eae7cc589bdd2de24e5e9
a1dotls <meeae9408@gmail.com>
2022-12-02 17:35:48 +0900

    각도기 문제 풀이 조건 연선자 사용

--- src/No120829/Solution.java
+++ src/No120829/Solution.java
@@ -2,26 +2,6 @@ package No120829;
 
 public class Solution {
     public int solution(int angle) {
-        int answer = 0;
-        boolean acuteValid = angle > 0 && angle < 90;
-        boolean rightValid = angle == 90;
-        boolean obtuseValid = angle > 90 && angle < 180;
-        boolean straightValid =  angle == 180;
-
-        if (acuteValid) {
-            return 1;
-        }
-        if (rightValid) {
-            return 2;
-        }
-        if (obtuseValid) {
-            return 3;
-        }
-        if (straightValid) {
-            return 4;
-        }
-
-        return answer;
+        return angle == 180 ? 4 : angle < 90 ? 1 : angle == 90 ? 2 : angle > 90 ? 3 : 0;
     }
-
 }
